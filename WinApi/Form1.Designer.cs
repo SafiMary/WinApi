e62d21e9cc910bc5fe386b65be237a6e24103ac6
ADMIN <ADMIN@DESKTOP-CBRV7QC>
2023-12-05 21:12:26 +0500

    Добавьте файлы проекта.

---
+++ WinApi/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WinApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.WinName = new System.Windows.Forms.TextBox();
+            this.NewCaption = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Window_s = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // WinName
+            // 
+            this.WinName.Location = new System.Drawing.Point(94, 204);
+            this.WinName.Name = "WinName";
+            this.WinName.Size = new System.Drawing.Size(100, 20);
+            this.WinName.TabIndex = 1;
+            // 
+            // NewCaption
+            // 
+            this.NewCaption.Location = new System.Drawing.Point(94, 259);
+            this.NewCaption.Name = "NewCaption";
+            this.NewCaption.Size = new System.Drawing.Size(100, 20);
+            this.NewCaption.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(713, 219);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Window_s
+            // 
+            this.Window_s.Location = new System.Drawing.Point(254, 53);
+            this.Window_s.Multiline = true;
+            this.Window_s.Name = "Window_s";
+            this.Window_s.Size = new System.Drawing.Size(408, 298);
+            this.Window_s.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Window_s);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.NewCaption);
+            this.Controls.Add(this.WinName);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox WinName;
+        private System.Windows.Forms.TextBox NewCaption;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox Window_s;
+    }
+}
+
---
+++ WinApi/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using System.Diagnostics;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WinApi
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(IntPtr hWnd, string Text, string Caption, int Options);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetWindowTextA(IntPtr hWnd, string Caption);
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindowW(string ClassName, string WindowName);
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, uint wParam, [MarshalAs(UnmanagedType.LPStr)] string Param);
+        const uint WM_SETEXT = 0x0C;
+
+        delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsWindowVisible(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        public static extern int GetClassName (IntPtr hWnd, StringBuilder ClassName,int Count);
+        private string GetClassName(IntPtr hWnd)
+        {
+            StringBuilder ClassName = new StringBuilder(256);
+            GetClassName(hWnd,ClassName, ClassName.Capacity);
+            return ClassName.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox(IntPtr.Zero, "Hello World!!", "Test", 0);
+            SetWindowTextA(this.Handle, "Поехали!!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IntPtr hWnd = FindWindowW(null, WinName.Text);
+            //SetWindowTextA(hWnd,NewCaption.Text);
+            SendMessage(hWnd, WM_SETEXT, 0, NewCaption.Text);
+        }
+    
+
+        string GetWindowText(IntPtr hWnd)
+        {
+            int len = GetWindowTextLength(hWnd) + 1;
+            StringBuilder sb = new StringBuilder(len);
+            len = GetWindowText(hWnd, sb, len);
+            return sb.ToString(0, len);
+        }
+    
+
+    private void button3_Click(object sender, EventArgs e)
+        {
+           EnumWindows((hWnd, lParam) => {
+                if (IsWindowVisible(hWnd) && GetWindowTextLength(hWnd) != 0)
+                {
+                    Window_s.Text+= "Класс= "+ GetClassName(hWnd) + " Название= "+ GetWindowText(hWnd)+"\r\n";
+                }
+                return true;
+            }, IntPtr.Zero);
+        }
+    }
+}
